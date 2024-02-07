e7812022620747819041f1cedc5d87c4cea2fd91
apb8176 <56334384+apb8176@users.noreply.github.com>
2024-02-07 18:08:25 +0530

    main ts

--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -1,7 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import amplifyconfig from '../amplifyconfiguration.json';
+import amplifyconfig from './amplifyconfiguration1.json';
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure(amplifyconfig);
