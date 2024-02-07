5bdb8e13658612544e6c260daecd4c9bcd18d722
apb8176 <56334384+apb8176@users.noreply.github.com>
2024-02-07 17:19:04 +0530

    amplify

--- hello-world/src/app/app.config.ts
+++ hello-world/src/app/app.config.ts
@@ -1,8 +1,8 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { Amplify } from 'aws-amplify';
 import amplifyconfig from '../amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify';
 Amplify.configure(amplifyconfig);
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
