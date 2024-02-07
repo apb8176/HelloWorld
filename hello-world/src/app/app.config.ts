71afbad0bad1a609503d6ac344958d23a06b7889
apb8176 <56334384+apb8176@users.noreply.github.com>
2024-02-07 16:57:29 +0530

    config

--- hello-world/src/app/app.config.ts
+++ hello-world/src/app/app.config.ts
@@ -1,8 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from '../amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
--- hello-world/src/main.ts
+++ hello-world/src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import amplifyconfig from '../amplifyconfiguration.json';
+import { Amplify } from 'aws-amplify';
+
+Amplify.configure(amplifyconfig);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
